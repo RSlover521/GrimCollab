b0c49c840da7c0f1596dae2d80fe8378418c419e
RSlover521 <wulance58@gmail.com>
2025-08-04 21:35:22 -0500

    recommented everything - Corrected Homework

--- Grim_demo_project/src/wu/MAIN/Xinchen.java
+++ Grim_demo_project/src/wu/MAIN/Xinchen.java
@@ -2,22 +2,25 @@ package wu.MAIN;
 
 public class Xinchen {
 	public static void main (String[] args) {
-		myFunc(197, true); // only take in a number
-		myFunc(521);
+		myFunc(197, true);
+		
+		myFunc(521); // improved method call
 		}
 	
-	public static void myFunc (int number, boolean test) { // well I meant it to return a boolean variable- Also only takes one integer variable
+	public static void myFunc (int number, boolean test) { // well I meant it to return a boolean variable- Also only takes in one integer variable as your parameter
 		if (number % 2 == 0) { // nice logic here
 			test = true;
 		} else {
 			test = false;
 		}
 		System.out.println(test);
+		
+		// your logic looks good, but what i asked for is returning a boolean. 
 	}
 	
 	
 	// here is the improved answer (yours is still correct, it still works, but it works differently than mine.
-	public static boolean myFunc (int num) {
+	public static boolean myFunc(int num) {
 		boolean result = false;
 		// if num is divisible by 2, result = true
 		if(num % 2 == 0) {
